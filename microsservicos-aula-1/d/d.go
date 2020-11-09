dd2ff29ae4c5075bbdb39fe09ce46588f3fc417d
tiagobani <tiago.bani@yahoo.com.br>
2020-11-09 07:58:05 -0300

    [feat]: add service d

---
+++ microsservicos-aula-1/d/d.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Result struct {
+	Status string
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":9093", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Teste do micro serviço d")
+
+	result := Result{Status: "accepted"}
+
+	jsonResult, err := json.Marshal(result)
+	if err != nil {
+		log.Fatal("Error converting json")
+	}
+
+	fmt.Fprintf(w, string(jsonResult))
+}
